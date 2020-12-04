c47502f525682999eb5765381d894b56bc20d077
iusmanof <iusmanof@gmail.com>
2020-12-04 10:51:51 +0300

    feat: app completed

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-function createTodoItem(title){
+function createTodoItem(title) {
     const checkbox = document.createElement('input');
     checkbox.type = 'checkbox';
+    checkbox.className = 'checkbox';
 
     const label = document.createElement('label');
     label.innerText = title;
@@ -13,21 +14,82 @@ function createTodoItem(title){
     const editButton = document.createElement('button');
     editButton.innerText = 'Изменить';
     editButton.className = 'edit';
+
+    const deleteButton = document.createElement('button');
+    deleteButton.innerText = 'Удалить';
+    deleteButton.className = 'delete';
+
+    const listItem = document.createElement('li');
+    listItem.className = 'todo-item';
+
+    listItem.appendChild(checkbox);
+    listItem.appendChild(label);
+    listItem.appendChild(editInput);
+    listItem.appendChild(editButton);
+    listItem.appendChild(deleteButton);
+
+    bindEvents(listItem);
+
+    return listItem;
 }
 
-function addTodoItem(event){
+function bindEvents(todoItem) {
+    const checkbox = todoItem.querySelector('.checkbox');
+    const editButton = todoItem.querySelector('button.edit');
+    const deleteButton = todoItem.querySelector('button.delete');
+
+
+    checkbox.addEventListener('change', toggleTodoItem);
+    editButton.addEventListener('click', editTodoItem);
+    deleteButton.addEventListener('click', deleteTodoItem);
+}
+
+function addTodoItem(event) {
     event.preventDefault(); // Остановить отправку данные на сервер
 
-    if(addInput.value === '') return alert('Необходимо ввсети название задачи')
+    if (addInput.value === '') return alert('Необходимо ввсети название задачи');
 
-    const listItem = createTodoItem(addInput.value);
+    const todoItem = createTodoItem(addInput.value);
+    todoList.appendChild(todoItem);
+    addInput.value = '';
+}
 
+function toggleTodoItem() { // {св-во обьекта = target} = event.target
+    // this = target
+    const listItem = this.parentNode; // parentNode = parentElement - родитель-элемент
+    listItem.classList.toggle('completed')
+}
 
+function editTodoItem() {
+    const listItem = this.parentNode;
+    const title = listItem.querySelector('.title');
+    const editInput = listItem.querySelector('.textfield');
+    const isEditing = listItem.classList.contains('editing');
+
+    if (isEditing) {
+        title.innerText = editInput.value;
+        this.innerText = 'Изменить';
+    } else {
+        editInput.value = title.innerText;
+        this.innerText = 'Сохранить';
+    }
+
+    listItem.classList.toggle('editing');
+}
+
+function deleteTodoItem() {
+    const listItem = this.parentNode;
+    todoList.removeChild(listItem);
 }
 
 const todoForm = document.getElementById('todo-form')
-const todoForm = document.getElementById('todo-form')
 const addInput = document.getElementById('add-input')
-const todoList = document.querySelectorAll('.todo-item')
+const todoList = document.getElementById('todo-list')
+const todoItems = document.querySelectorAll('.todo-item')
 
-todoForm.addEventListener('submit', addTodoItem);
+function main() {
+    todoForm.addEventListener('submit', addTodoItem);
+    todoItems.forEach(item => bindEvents(item));
+}
+
+main();
